285ccacab869fa480807062cb14300cc6b0351c7
Anthony J Dye <dye0118@gmail.com>
2022-03-15 10:27:44 -0500

    added project_1 file

---
+++ project_1.py
@@ -0,0 +1,42 @@
+# Day Trip Generator
+
+#(5 points): As a developer, I want to store my destinations, restaurants, mode of transportations, and entertainments in their own separate lists.
+#(5 points): As a user, I want a destination to be randomly selected for my day trip.
+#(5 points): As a user, I want a restaruant to be randomly selected for my day trip.
+#(5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+#(5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+#(15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don't like one.
+#(10 points): As a user, I want to be able to confirm that my  day trip is "complete" once I like all of the random selections.
+#(10 points): As a user, I want to display my completed trip in the console.
+#(5 points): As a developer, I want all of my funtions to have a Single Responsibility. Remember, each function should do just one thing!
+
+import random
+
+destinations = ['New York City', 'Berlin', 'Amsterdam', 'Tokyo', 'Barcelona']
+restaurants_new_york_city = ["Delmonico's", "Lombardi's", 'Keens Steakhouse', 'The Rainbow Room', "Katz's Delicatessen"]
+restaurants_berlin = ['Skykitchen', 'Hasty Pastry', 'Burger Turm', 'Hackethals', 'Sapori di Casa']
+restaurants_amsterdam = ['Baaz Amsterdam', 'The Pantry', 'Chez Lorraine', 'Restaurant Bougainville', 'Ta Toru']
+restaurants_tokyo = ['Gyopa Gyoza Roppongi', 'Jiromaru Akihabara', 'Ise Sueyohsi', 'Han no Daidokoro Honten', 'Ninja Shinjuku Ajito']
+restaurants_barcelona = ['BelleBuon', 'La Pasta Lab', 'Bodega Biarritz', 'Locavore Barcelona', 'Sumac & Mambo']
+modes_of_transportation = ['Rental Car', 'Train', "Bicycle", 'Taxi', 'Walking' ]
+list_of_entertainment_new_york_city = ['Showtime at the Apollo', 'Manhattan Sky Tour', '9/11 Ground Zero Tour','One World Observatory', 'Times Square'  ]
+list_of_entertainment_berlin = ['Love Parade', 'Berlin Icebar Experience', 'Cosmic Comedy Club', 'Tresor', 'Sachsenhausen Concentration Camp Memorial Tour']
+list_of_entertainment_amsterdam = ['Red Light District', 'Anne Frank House', 'Vondelpark', "CoffeeShops", 'Heineken Experience' ]
+list_of_entertainment_tokyo = ['Climb Mt. Fuji', 'Fight Ninjas', 'Tokyo Disney', 'Otaku Tour', 'Tokyo Tower']
+list_of_entertainment_barcelona = ['Running of the Bulls','Beaches', 'Tapas and Wine Experience', 'CosmoCaxia', 'Erotica Museum of Barcelona' ]
+
+def display_greeting()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
