00e1577b256f7c9e9863f07aadeefe724ae1d06d
Dye <anthony.dye@ies-co.com>
2022-03-16 11:06:21 -0500

    changed the lists

--- project_1.py
+++ project_1.py
@@ -11,56 +11,13 @@
 
 import random
 
-destinations = ['New York City', 'Berlin', 'Amsterdam', 'Tokyo', 'Barcelona']
-restaurants_new_york_city = ["Delmonico's", "Lombardi's", 'Keens Steakhouse', 'The Rainbow Room', "Katz's Delicatessen"]
-restaurants_berlin = ['Skykitchen', 'Hasty Pastry', 'Burger Turm', 'Hackethals', 'Sapori di Casa']
-restaurants_amsterdam = ['Baaz Amsterdam', 'The Pantry', 'Chez Lorraine', 'Restaurant Bougainville', 'Ta Toru']
-restaurants_tokyo = ['Gyopa Gyoza Roppongi', 'Jiromaru Akihabara', 'Ise Sueyohsi', 'Han no Daidokoro Honten', 'Ninja Shinjuku Ajito']
-restaurants_barcelona = ['BelleBuon', 'La Pasta Lab', 'Bodega Biarritz', 'Locavore Barcelona', 'Sumac & Mambo']
-modes_of_transportation = ['Rental Car', 'Train', "Bicycle", 'Taxi', 'Walking' ]
-list_of_entertainment_new_york_city = ['Showtime at the Apollo', 'Manhattan Sky Tour', '9/11 Ground Zero Tour','One World Observatory', 'Times Square'  ]
-list_of_entertainment_berlin = ['Love Parade', 'Berlin Icebar Experience', 'Cosmic Comedy Club', 'Tresor', 'Sachsenhausen Concentration Camp Memorial Tour']
-list_of_entertainment_amsterdam = ['Red Light District', 'Anne Frank House', 'Vondelpark', "CoffeeShops", 'Heineken Experience' ]
-list_of_entertainment_tokyo = ['Climb Mt. Fuji', 'Fight Ninjas', 'Tokyo Disney', 'Otaku Tour', 'Tokyo Tower']
-list_of_entertainment_barcelona = ['Running of the Bulls','Beaches', 'Tapas and Wine Experience', 'CosmoCaxia', 'Erotica Museum of Barcelona' ]
+destinations = ['Bikini Bottom', 'Townsville', 'International Space Station', 'Stormwind', 'Silvermoon City'  ]
+list_of_restaurants = ["Krusty Krab", "Good Burger", "Soul Food Cafe", "Chuck E. Cheese", "Tiana's Palace"]
+modes_of_transportation = ['Rickshaw', 'Train', "Bicycle", 'Horse & Buggy', 'Walking' ]
+list_of_entertainment = ['Criminal Activity', 'Jellyfishing', 'General Hooliganism', 'Go to a museum', 'Poke a bear']
 
-print('Welcome to the day trip generator, where your trip will be planned out for you.')
-
-def select_entertainment(list_of_entertainment_new_york_city):
-        random_entertainment = random.choice(list_of_entertainment_new_york_city)
-        print(f'We have selected {random_entertainment} for your enjoyment!')
-        answer = input('Does this selection work for you? [y/n]: ')
-        while answer == 'n':
-            random_entertainment = random.choice(list_of_entertainment_new_york_city)
-            print(f"Sorry that destination doesn't work for you, how about {random_entertainment}?")
-            answer = input('Does this selection work for you? [y/n]: ')
-        if answer == 'y':
-            print(f'Glorious! Have a great time!')
-        return random_entertainment
-
-def select_random_restaurant(restaurants_new_york_city):
-        random_restaurant = random.choice(restaurants_new_york_city)
-        print(f'We have selected {random_restaurant} for your dining needs!')
-        answer = input('Does this selection work for you? [y/n]: ')
-        while answer == 'n':
-            random_restaurant = random.choice(restaurants_new_york_city)
-            print(f"Sorry that destination doesn't work for you, how about {random_restaurant}?")
-            answer = input('Does this selection work for you? [y/n]: ')
-        if answer == 'y':
-            print(f'Sweet! Very nice choice. Bon Apetit!')
-        return random_restaurant
-
-def select_random_restaurant(restaurants_berlin):
-        random_restaurant = random.choice(restaurants_berlin)
-        print(f'We have selected {random_restaurant} for your dining needs!')
-        answer = input('Does this selection work for you? [y/n]: ')
-        while answer == 'n':
-            random_restaurant = random.choice(restaurants_berlin)
-            print(f"Sorry that destination doesn't work for you, how about {random_restaurant}?")
-            answer = input('Does this selection work for you? [y/n]: ')
-        if answer == 'y':
-            print(f'Sweet! Very nice choice. Bon Apetit!')
-        return random_restaurant
+def display_greeting():
+    print('Welcome to the day trip generator, where your trip will be planned out for you.')
 
 def select_random_destination(destinations):
     random_destination = random.choice(destinations)
@@ -72,30 +29,59 @@ def select_random_destination(destinations):
         answer = input('Does this selection work for you? [y/n]: ')
     if answer == 'y':
         print(f"Awesome! We got that selected for you. Let's keep going!")
-        return random_destination
-random_destination = select_random_destination(destinations)
-if random_destination == 'New York City':
-        select_random_restaurant(restaurants_new_york_city)
-elif random_destination == 'Berlin':
-        select_random_restaurant(restaurants_berlin)
-elif random_destination == 'Barcelona':
-        select_random_restaurant(restaurants_barcelona)
-elif random_destination == 'Amsterdam':
-        select_random_restaurant(restaurants_amsterdam)
-else:
-        select_random_restaurant(restaurants_tokyo)
+    return random_destination
+def select_random_restaurant(list_of_restaurants):
+    random_restaurant = random.choice(list_of_restaurants)
+    print(f'We have selected {random_restaurant} for your dining needs!')
+    answer = input('Does this selection work for you? [y/n]: ')
+    while answer == 'n':
+        random_restaurant = random.choice(list_of_restaurants)
+        print(f"Sorry that restaurant doesn't work for you, how about {random_restaurant}?")
+        answer = input('Does this selection work for you? [y/n]: ')
+    if answer == 'y':
+        print(f"Sweet! We got that selected for you. Bon Apetit! Let's keep going!")
+    return random_restaurant
+def select_random_entertainment(list_of_entertainment):
+    random_entertainment = random.choice(list_of_entertainment)
+    print(f'We have selected {random_entertainment} for you to enjoy!')
+    answer = input('Does this selection work for you? [y/n]: ')
+    while answer == 'n':
+        random_entertainment = random.choice(list_of_entertainment)
+        print(f"Sorry that entertainment doesn't work for you, how about {random_entertainment}?")
+        answer = input('Does this selection work for you? [y/n]: ')
+    if answer == 'y':
+        print(f"Glorious! I hope you have a wonderful time! Let's keep going!")
+    return random_entertainment
+def select_random_transportation(mode_of_transportation):
+    random_transportation = random.choice(mode_of_transportation)
+    print(f'We have selected {random_transportation} for your mobile needs!')
+    answer = input('Does this selection work for you? [y/n]: ')
+    while answer == 'n':
+        random_transportation = random.choice(mode_of_transportation)
+        print(f"Sorry that transportation doesn't work for you, how about {random_transportation}?")
+        answer = input('Does this selection work for you? [y/n]: ')
+    if answer == 'y':
+        print(f"Perfect! Now you can move around while you're there. Let's keep going!")
+    return random_transportation
+def trip_comfirmation(destination, transportation, entertainment, restaurant):
+    print('------------------------------------------------------------------')
+    print(f'You will be going to {destination}.') 
+    print(f'Where you will be traveling by {transportation}.')
+    print(f'While you are there you will be eating at {restaurant}') 
+    print(f'And you will be partaking in {entertainment} for your enjoyment')
+    print('------------------------------------------------------------------')
+    answer = input('Does this itenerary work for you? [y/n]: ')
+    if answer == 'y':
+        print('Have an amazing trip. Thank you for using our Random Day Trip Generator!')
+    elif answer == 'n':
+        run_day_trip
+def run_day_trip():
+    display_greeting()
+    confirmed_destination = select_random_destination(destinations)
+    confrimed_transportation = select_random_transportation(modes_of_transportation)
+    confirmed_entertainment = select_random_entertainment(list_of_entertainment)
+    confirmed_restaurant = select_random_restaurant(list_of_restaurants)
+    trip_comfirmation(confirmed_destination, confrimed_transportation, confirmed_entertainment, confirmed_restaurant)
 
-
-select_random_destination(destinations)
-select_random_restaurant(restaurants_new_york_city)
-select_random_restaurant(restaurants_berlin)
-select_random_restaurant(restaurants_amsterdam)
-select_random_restaurant(restaurants_barcelona)
-select_random_restaurant(restaurants_tokyo)
-select_entertainment(list_of_entertainment_new_york_city)
-select_entertainment(list_of_entertainment_amsterdam)
-select_entertainment(list_of_entertainment_barcelona)
-select_entertainment(list_of_entertainment_berlin)
-select_entertainment(list_of_entertainment_tokyo)
-
+run_day_trip()
 
