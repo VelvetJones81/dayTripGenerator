176f35016599fca939f9fd01429b0a8a192846a3
Dye <anthony.dye@ies-co.com>
2022-03-16 08:37:50 -0500

    added functions to project

--- project_1.py
+++ project_1.py
@@ -1,5 +1,4 @@
 # Day Trip Generator
-
 #(5 points): As a developer, I want to store my destinations, restaurants, mode of transportations, and entertainments in their own separate lists.
 #(5 points): As a user, I want a destination to be randomly selected for my day trip.
 #(5 points): As a user, I want a restaruant to be randomly selected for my day trip.
@@ -25,18 +24,89 @@ list_of_entertainment_amsterdam = ['Red Light District', 'Anne Frank House', 'Vo
 list_of_entertainment_tokyo = ['Climb Mt. Fuji', 'Fight Ninjas', 'Tokyo Disney', 'Otaku Tour', 'Tokyo Tower']
 list_of_entertainment_barcelona = ['Running of the Bulls','Beaches', 'Tapas and Wine Experience', 'CosmoCaxia', 'Erotica Museum of Barcelona' ]
 
-def display_greeting()
+print('Welcome to the day trip generator, where your trip will be planned out for you.')
+
+def select_entertainment(list_of_entertainment_new_york_city):
+        random_entertainment = random.choice(list_of_entertainment_new_york_city)
+        print(f'We have selected {random_entertainment} for your enjoyment!')
+        answer = input('Does this selection work for you? [y/n]: ')
+        while answer == 'n':
+            random_entertainment = random.choice(list_of_entertainment_new_york_city)
+            print(f"Sorry that destination doesn't work for you, how about {random_entertainment}?")
+            answer = input('Does this selection work for you? [y/n]: ')
+        if answer == 'y':
+            print(f'Glorious! Have a great time!')
+        return random_entertainment
+
+def select_random_restaurant(restaurants_new_york_city):
+        random_restaurant = random.choice(restaurants_new_york_city)
+        print(f'We have selected {random_restaurant} for your dining needs!')
+        answer = input('Does this selection work for you? [y/n]: ')
+        while answer == 'n':
+            random_restaurant = random.choice(restaurants_new_york_city)
+            print(f"Sorry that destination doesn't work for you, how about {random_restaurant}?")
+            answer = input('Does this selection work for you? [y/n]: ')
+        if answer == 'y':
+            print(f'Sweet! Very nice choice. Bon Apetit!')
+        return random_restaurant
+
+def select_random_restaurant(restaurants_berlin):
+        random_restaurant = random.choice(restaurants_berlin)
+        print(f'We have selected {random_restaurant} for your dining needs!')
+        answer = input('Does this selection work for you? [y/n]: ')
+        while answer == 'n':
+            random_restaurant = random.choice(restaurants_berlin)
+            print(f"Sorry that destination doesn't work for you, how about {random_restaurant}?")
+            answer = input('Does this selection work for you? [y/n]: ')
+        if answer == 'y':
+            print(f'Sweet! Very nice choice. Bon Apetit!')
+        return random_restaurant
+
+def select_random_destination(destinations):
+    random_destination = random.choice(destinations)
+    print(f'We have selected {random_destination} for you!')
+    answer = input('Does this selection work for you? [y/n]: ')
+    while answer == 'n':
+        random_destination = random.choice(destinations)
+        print(f"Sorry that destination doesn't work for you, how about {random_destination}?")
+        answer = input('Does this selection work for you? [y/n]: ')
+    if answer == 'y':
+        print(f"Awesome! We got that selected for you. Let's keep going!")
+        return random_destination
+random_destination = select_random_destination(destinations)
+if random_destination == 'New York City':
+        select_random_restaurant(restaurants_new_york_city)
+elif random_destination == 'Berlin':
+        select_random_restaurant(restaurants_berlin)
+elif random_destination == 'Barcelona':
+        select_random_restaurant(restaurants_barcelona)
+elif random_destination == 'Amsterdam':
+        select_random_restaurant(restaurants_amsterdam)
+else:
+        select_random_restaurant(restaurants_tokyo)
 
 
+select_random_destination(destinations)
+select_random_restaurant(restaurants_new_york_city)
+select_random_restaurant(restaurants_berlin)
+select_random_restaurant(restaurants_amsterdam)
+#select_random_restaurant(restaurants_barcelona)
+#select_random_restaurant(restaurants_tokyo)
+select_entertainment(list_of_entertainment_new_york_city)
+#select_entertainment(list_of_entertainment_amsterdam)
+#select_entertainment(list_of_entertainment_barcelona)
+select_entertainment(list_of_entertainment_berlin)
+#select_entertainment(list_of_entertainment_tokyo)
 
-
-
-
-
-
-
-
-
-
-
-
+#def select_random_restaurant(restaurants):
+ #       random_restaurant = random.choice(restaurants)
+  #      print(f'We have selected {random_restaurant} for your dining needs!')
+   #     answer = input('Does this selection work for you? [y/n]: ')
+    #    while answer == 'n':
+     #       random_restaurant = random.choice(restaurants)
+      #      print(f"Sorry that destination doesn't work for you, how about {random_restaurant}?")
+       #     answer = input('Does this selection work for you? [y/n]: ')
+        #print(f'Sweet! Very nice choice. Bon Apetit!')
+        #return random_restaurant
+#random_nyc = select_random_restaurant(restaurants_new_york_city)
+#print(random_nyc)
